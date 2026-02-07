f2c08740c9dcb77c97ab69c1d1f509b02732dfa8
TigRig <tigrig.crex@gmail.com>
2026-02-07 22:30:31 +0900

    背景透過設定

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    opts = {
+      transparent = true, -- ここを true にする
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
