650f1bd3128230d119786718c23103a045cd422a
TigRig <tigrig.crex@gmail.com>
2026-02-08 21:09:33 +0900

    ウィンドウの境界線を見やすい色に設定

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,11 +3,19 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     opts = {
-      transparent = true, -- ここを true にする
+      -- 透過設定
+      transparent = true,
       styles = {
         sidebars = "transparent",
         floats = "transparent",
       },
+      on_highlights = function(hl, c)
+        -- ウィンドウの境界線を見やすくする
+        hl.WinSeparator = {
+          fg = c.border_highlight, -- カラースキームが持つ強調色を使用
+          bold = true,
+        }
+      end,
     },
   },
 }
