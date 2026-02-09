92d5e5b2ef85a222a428b565f18a2c7256aaede0
TigRig <tigrig.crex@gmail.com>
2026-02-09 23:07:21 +0900

    インサートモードでCtrl+V貼り付け

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- インサートモードで Ctrl + V を押した時にクリップボードの内容を貼り付ける
+vim.keymap.set("i", "<C-v>", "<C-r><C-o>+", { desc = "Paste from system clipboard" })
+-- コマンドラインモード（: を押した後）での貼り付け
+vim.keymap.set("c", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard" })
