8ff93fa7844b1126e5ff9410eb382225f48733a9
TigRig <tigrig.crex@gmail.com>
2026-02-11 21:54:04 +0900

    コメントの自動継続を無効化

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- コメントの自動継続を無効化
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    -- r: Enter時の継続をオフ
+    -- o: o, Oでの継続をオフ
+    vim.opt_local.formatoptions:remove({ "r", "o" })
+  end,
+})
