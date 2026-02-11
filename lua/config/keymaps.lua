ef09a1aec6ea8015f752b09f4d3d69b655ca2cbb
TigRig <tigrig.crex@gmail.com>
2026-02-11 21:02:52 +0900

    ターミナル表示のキーマップ変更

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,13 @@
 vim.keymap.set("i", "<C-v>", "<C-r><C-o>+", { desc = "Paste from system clipboard" })
 -- コマンドラインモード（: を押した後）での貼り付け
 vim.keymap.set("c", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard" })
+
+-- ターミナルキー設定
+vim.keymap.del("n", "<leader>ft")
+vim.keymap.del("n", "<leader>fT")
+vim.keymap.set("n", "<leader>t", function()
+  Snacks.terminal()
+end, { desc = "Terminal (Root Dir)" })
+vim.keymap.set("n", "<leader>T", function()
+  Snacks.terminal(nil, { cwd = vim.uv.cwd() })
+end, { desc = "Terminal (cwd)" })
