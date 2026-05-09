584be40d60d72e99d80870381f17975c19bdf7f6
TigRig <tigrig.crex@gmail.com>
2026-05-09 10:43:42 +0900

    snacks.explorer -> oil.nvim

---
+++ lua/plugins/explorer.lua
@@ -0,0 +1,38 @@
+return {
+  -- snacks.explorer の無効化とキーマップの削除
+  {
+    "folke/snacks.nvim",
+    opts = {
+      explorer = { enabled = false },
+      picker = { explorer = { enabled = false } },
+    },
+    keys = {
+      -- デフォルトのキーマップを無効化
+      { "<leader>e", false },
+      { "<leader>E", false },
+    },
+  },
+  -- oil.nvim の追加と設定
+  {
+    "stevearc/oil.nvim",
+    -- 起動時に確実に読み込む
+    lazy = false,
+    opts = {
+      -- デフォルトで netrw を置き換える
+      default_file_explorer = true,
+      -- アイコン表示の設定
+      columns = {
+        "icon",
+      },
+      -- プレビュー設定など
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+      { "<leader>e", "<cmd>Oil<cr>", desc = "Open file explorer (Oil)" },
+    },
+  },
+}
