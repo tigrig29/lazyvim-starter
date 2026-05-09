07ff170c23c36ae14cfe054799fcbd04bfa7e3f7
TigRig <tigrig.crex@gmail.com>
2026-05-09 16:01:34 +0900

    ﻿config: ターミナルのトグルキーバインドを <leader><leader> に変更

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,9 +10,12 @@ vim.keymap.set("c", "<C-v>", "<C-r>+", { desc = "Paste from system clipboard" })
 -- ターミナルキー設定
 vim.keymap.del("n", "<leader>ft")
 vim.keymap.del("n", "<leader>fT")
-vim.keymap.set("n", "<leader>t", function()
+vim.keymap.set("n", "<leader><leader>", function()
   Snacks.terminal()
 end, { desc = "Terminal (Root Dir)" })
 vim.keymap.set("n", "<leader>T", function()
   Snacks.terminal(nil, { cwd = vim.uv.cwd() })
 end, { desc = "Terminal (cwd)" })
+
+-- <C-s> で保存
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
