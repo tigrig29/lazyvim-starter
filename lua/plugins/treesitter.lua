e89a313d3c6976c6fda463089c5a02d63099e74b
TigRig <tigrig.crex@gmail.com>
2026-05-18 14:00:37 +0900

    nvim-treesitterを管理専用ツールとして再導入

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,18 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    enabled = false,
+    enabled = true,
+    build = ":TSUpdate",
+    opts = function(_, opts)
+      -- C# (c_sharp) が確実にインストールされるようにする
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "c_sharp" })
+
+      -- 【重要】プラグイン側の機能はすべてオフにし、ネイティブ(Neovim本体)に任せる
+      opts.highlight = { enable = false }
+      opts.indent = { enable = false }
+      opts.incremental_selection = { enable = false }
+    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
